39851d6ee2a7b0c7e2e3dbd633b46f0cb600f7e1
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2026-03-14 15:18:14 +0500

    tabControl SelectedIndex started

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -24,10 +24,13 @@ namespace Academy
             movies_connector = new Connector("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_SPU_411;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             //dgvDirections.DataSource = movies_connector.Select("SELECT [№\nп/п] = movie_id,[Название фильма] = title,[Режиссер] = FORMATMESSAGE(N'%s %s', first_name,last_name) FROM Movies, Directors WHERE director = director_id ORDER BY movie_id");
             //dgvDirections.DataSource = movies_connector.Select("SELECT * FROM Movies");
+        }
+
+        private void tabControl_SelectedIndexChanged(object sender, EventArgs e)
+        {
             dgvDirections.DataSource = connector.Select("SELECT * FROM Directions");
             toolStripStatusLabel.Text = $"Количество направлений обучения: {dgvDirections.RowCount-1}";
             //toolStripStatusLabel.Text = $"Количество направлений обучения: {connector.Scalar("SELECT COUNT(*) FROM Directions")}";
-
         }
     }
 }
